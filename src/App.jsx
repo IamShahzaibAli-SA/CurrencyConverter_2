2610050ff3ad35879de95e08d1fa54a9a8b28d1d
IamShahzaibAli-SA <243137271+IamShahzaibAli-SA@users.noreply.github.com>
2025-11-20 06:21:43 -0800

    Checkpoint 2: Start Writing Your Code From Here

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,34 @@ function App() {
 
   return (
     <>
-      
+      <div className='flex justify-center'>
+      <div className='w-[700px] p-5 bg-gray-500'>
+        <h1 className='text-white text-[30px]'>Currency Converter</h1>
+
+        <div>
+          <p className='text-white text-[20px]'>Enter Amount</p>
+          <input
+            className='bg-white text-black pl-2'
+            type="number"
+            value={amount}
+          />
+        </div>
+
+        <div>
+          <p>Convert From</p>
+          
+        </div>
+
+        <div>
+          <p>Convert To</p>
+          
+        </div>
+
+        <p className='text-white text-[20px]'>
+          Converted Amount: 
+        </p>
+      </div>
+    </div>
     </>
   )
 }
