9a91f10b10bb418e2069ac33be86a02c7d6e3a74
IamShahzaibAli-SA <243137271+IamShahzaibAli-SA@users.noreply.github.com>
2025-11-20 07:09:49 -0800

    Hook Is Finally Ready

--- src/hooks/useCurrencyAPI.js
+++ src/hooks/useCurrencyAPI.js
@@ -3,16 +3,15 @@ import { useEffect, useState } from "react";
 function useCurrencyAPI(currency) {
     const [data, setData] = useState(null);
 
+    const fetchCurrency = async () => {
+        const url = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`;
+        const response = await fetch(url);
+        const result = await response.json();
+        setData(result[currency]);
+    }
+
     useEffect(() => {
-        const fetchCurrency = async () => {
-            const url = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`;
-            const response = await fetch(url);
-            const result = response.json();
-            setData(result[currency]);
-        }
-
         fetchCurrency();
-
     }, [currency])
 
     return data;
